2b8b79c708a1d65cc6acb3c5e3e9ed4b39215694
RitaMelo18 <aritaamelo@gmail.com>
2026-01-29 09:38:02 +0000

    Correct CWE-611 sink

--- app/users/services/XmlProcessingService.scala
+++ app/users/services/XmlProcessingService.scala
@@ -1,22 +1,21 @@
 package users.services
 
 import scala.xml.Elem
+import javax.xml.parsers.SAXParserFactory
 
 private[users] class XmlProcessingService {
-  
-  /**
-   * Parses an XML string into an XML element.
-   * This service is used for processing user-submitted XML documents.
-   * 
-   * @param xmlString The XML string to parse
-   * @return The parsed XML element
-   */
+
   def parseXml(xmlString: String): Elem = {
     require(xmlString != null && xmlString.nonEmpty, "XML string cannot be null or empty")
-    
+    // Configure an insecure SAX parser (allows XXE)
+    val factory = SAXParserFactory.newInstance()
+    factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false)
+    factory.setFeature("http://xml.org/sax/features/external-general-entities", true)
+    factory.setFeature("http://xml.org/sax/features/external-parameter-entities", true)
+    val saxParser = factory.newSAXParser()
+
     //CWE-611
     //SINK
-    scala.xml.XML.loadString(xmlString)
+    scala.xml.XML.withSAXParser(saxParser).loadString(xmlString)
   }
-  
 }
